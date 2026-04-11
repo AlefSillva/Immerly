ea50c6ec85918c956b183e44267d29ead9c884ce
Alef Santos <alef.santos@al.infnet.edu.br>
2026-04-11 20:12:29 -0300

    feat: update App component to include Register route and change global CSS import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
+        <Route path="/register" element={ <Register/> } />
       </Routes>
     </BrowserRouter>
   )
