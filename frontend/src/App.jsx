868d163a9dfa2fa6257dc82f4d16f097db9170ee
Alef Santos <alef.santos@al.infnet.edu.br>
2026-04-11 21:38:16 -0300

    fix: correct token retrieval from localStorage in API interceptor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Register from './pages/Register'
+import Register from './pages/register/Register'
+import Login from './pages/login/Login';
+import RotaPrivada from './components/RotaPrivada';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={ <Register/> } />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        
+        <Route path="/dashboard" element={ 
+          <RotaPrivada>
+            <h1>Dashboard</h1>
+          </RotaPrivada>
+        } />
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,7 +7,7 @@ const api = axios.create({
 
 // Interceptador que injeta o token JWT em toda requisição automaticamente
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem(token);
+    const token = localStorage.getItem('token');
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
