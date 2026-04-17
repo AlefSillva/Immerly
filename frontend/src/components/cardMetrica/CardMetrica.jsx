e7e12ff2c1a7681b0eea16fd46b42c855d9bba64
Alef Santos <alef.santos@al.infnet.edu.br>
2026-04-17 18:17:28 -0300

    feat: implement CardMetrica component with layout and styling

--- frontend/src/components/cardMetrica/CardMetrica.jsx
+++ frontend/src/components/cardMetrica/CardMetrica.jsx
@@ -1,9 +1,16 @@
-import styles from './CardMetrica.module.css';
+import styles from "./CardMetrica.module.css";
 
-function Dashboard() {
+function CardMetrica({ titulo, valor, sufixo }) {
     return (
-        <div className={ styles.card }></div>
-    );
+        <div className={ styles.card }>
+            <span className={styles.titulo}>{ titulo }</span>
+                
+            <span className={styles.valor}>
+                { valor }
+                { sufixo && <span className={ styles.sufixo }>{ sufixo }</span>}
+            </span>
+        </div>
+    )
 }
 
-export default Dashboard;
+export default CardMetrica;
