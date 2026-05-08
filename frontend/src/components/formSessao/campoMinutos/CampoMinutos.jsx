776a0e1685bc1e2cfcb256f0b5bd2efbf6549b60
Alef Santos <alef.santos@al.infnet.edu.br>
2026-05-08 18:46:52 -0300

    style: update form components and dashboard styles for improved layout and user experience; adjust padding, margins, and colors

--- frontend/src/components/formSessao/campoMinutos/CampoMinutos.jsx
+++ frontend/src/components/formSessao/campoMinutos/CampoMinutos.jsx
@@ -4,17 +4,19 @@ function CampoMinutos({ label, name, value, onChange, required }) {
     return (
         <div className={styles.grupo}>
             <label className={ styles.label }>{ label }</label>
-            <input 
-                className={ styles.input }
-                type="number" 
-                name={ name } 
-                value={ value } 
-                onChange={onChange} 
-                min="1"
-                max="600"
-                placeholder='ex: 30'
-                required={ required }
-            /><span className={ styles.sufixo }>min</span>
+            <div className={ styles.containerInput }>
+                <input
+                    className={ styles.input }
+                    type="number"
+                    name={ name }
+                    value={ value }
+                    onChange={onChange}
+                    min="1"
+                    max="600"
+                    placeholder='ex: 30'
+                    required={ required }
+                /><span className={ styles.sufixo }>min.</span>
+            </div>
         </div>
     );
 }
