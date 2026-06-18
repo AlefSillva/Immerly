65ecf848200c11e3cc016824f38bae6d851edd7c
Alef Santos <alef.santos@al.infnet.edu.br>
2026-06-18 18:22:14 -0300

    feat: update database connection logic to use DATABASE_URL and improve .gitignore for environment files

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,17 @@ require('dotenv').config({
     path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 });
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+const pool = process.env.DATABASE_URL
+    ? new Pool({
+        conectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false}
+    })
+    : new Pool({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
 });
 
 module.exports = pool;
